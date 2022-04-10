b7099906570c3a79d93a84750effdca119fd3bfe
minugrammer0725 <kwawkminoo@gmail.com>
2022-04-10 13:06:17 -0700

    use router.push/replace to programmatically navigate

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,23 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
+  const router = useRouter();
+  console.log(router.query);
+
+  const loadProjectHandler = () => {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "max",
+        clientprojectid: "project_a",
+      },
+    });
+  };
+
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
